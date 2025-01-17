11019e14ad56c20c0d90a74b1f565ac3fd140a75
Christian Agyapong <Christianagyapong2023@gmail.com>
2025-01-17 18:18:53 +0000

    Additional file with new text in the old files

---
+++ Animals.java
@@ -0,0 +1,24 @@
+class Animals {
+    public void  Main() {
+        System.out.println( "The Animals makes a sound" );
+    }
+}
+    
+
+class Cat extends Animals {
+        public void  Main() {
+            System.out.println( "The Cat makes a sound" );
+        }
+    }
+ 
+    
+
+   
+ class Dog extends Animals {
+        public void  Main() {
+            System.out.println( "The Dog makes a sound" );
+        }
+    }
+
+    // Main class moved to Main.java
+
