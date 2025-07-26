450c867305b9ec6ab6f199d987273826fc35e420
Christian Agyapong <Christianagyapong2023@gmail.com>
2025-07-26 10:35:35 +0000

    Update Animals.java

--- Animals.java
+++ Animals.java
@@ -1,24 +1,35 @@
-class Animals {
-    public void  Main() {
-        System.out.println( "The Animals makes a sound" );
+// Animal.java
+public class Animal {
+    public void makeSound() {
+        System.out.println("The animal makes a sound");
     }
 }
-    
 
-class Cat extends Animals {
-        public void  Main() {
-            System.out.println( "The Cat makes a sound" );
-        }
+// Cat.java
+public class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("The cat makes a sound");
     }
- 
-    
+}
 
-   
- class Dog extends Animals {
-        public void  Main() {
-            System.out.println( "The Dog makes a sound" );
-        }
+// Dog.java
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("The dog makes a sound");
     }
+}
 
-    // Main class moved to Main.java
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();
 
+        myAnimal.makeSound();
+        myCat.makeSound();
+        myDog.makeSound();
+    }
+}
